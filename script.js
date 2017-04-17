45927981be9852cd593c56a52c072fce6c60019d
Bryn Edades <bryn.edades@gmail.com>
2017-04-17 19:34:26 +0800

    Added the function showStudents to show the list of students in script.js

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ function startRequest(){
     };
     classesRequest.send();
 }
+
 function showClasscodes(classesResponseText) {
     localStorage.setItem("classes", classesResponseText);
     varClasses = localStorage.getItem("classes");
@@ -80,6 +81,7 @@ function showClasscodes(classesResponseText) {
     selectionBox.appendChild(selection);
     selection.addEventListener("change", showClassDays);
 } 
+
 function showClassDays() {
     selectwhat = document.getElementById("selectwhat");
     selectwhat.innerHTML = "Select a class day . . .";
@@ -126,6 +128,7 @@ function showClassDays() {
     
     classDaysList.addEventListener("change", showClassHours);
 } 
+
 function showClassHours() {
     selectwhat = document.getElementById("selectwhat");
     selectwhat.innerHTML = "Select a class hour . . .";
@@ -182,3 +185,143 @@ function showClassHours() {
 		selectionBox.appendChild(classHourList);
 	}
 } 
+
+function showStudents(){
+    selectwhat = document.getElementById("selectwhat");
+    selectwhat.innerHTML = "";
+
+    classtable = document.getElementById("studentlist");
+    if(classtable){
+        selectionBox.removeChild(studentlist);
+    }
+
+	seletionBox = document.getElementById("selectionbox");
+
+    selection = document.getElementById("classcodelist");
+    classlist = document.getElementById("classlist");
+    selectedClasscode = selection.value;
+	selectedClassday = classlist.value;
+    selectedClasshour = this.value;
+
+    studentList = document.createElement("table");
+	studentList.setAttribute("id", "studentlist");
+
+    varRow = document.createElement("tr");
+
+    varHeading1 = document.createElement("th");
+    varHeadingText1 = document.createTextNode("idno");
+    varHeading1.appendChild(varHeadingText1);
+    varRow.appendChild(varHeading1);
+
+    varHeading2 = document.createElement("th");
+    varHeadingText2 = document.createTextNode("name");
+    varHeading2.appendChild(varHeadingText2);
+    varRow.appendChild(varHeading2);
+
+    varHeading3 = document.createElement("th");
+    varHeadingText3 = document.createTextNode("demerit");
+    varHeading3.appendChild(varHeadingText3);
+    varRow.appendChild(varHeading3);
+
+    varHeading4 = document.createElement("th");
+    varHeadingText4 = document.createTextNode("absent");
+    varHeading4.appendChild(varHeadingText4);
+    varRow.appendChild(varHeading4);
+
+    varHeading5 = document.createElement("th");
+    varHeadingText5 = document.createTextNode("report");
+    varHeading5.appendChild(varHeadingText5);
+    varRow.appendChild(varHeading5);
+
+    studentList.appendChild(varRow);
+
+    for(x=0; x<classes.length; x++){
+        if(selectedClasscode == classes[x].classcode){
+            varClasscode = classes[x].classcode;
+            for(y=0; y<classes[x].classdays.length; y++){
+                if(selectedClassday == classes[x].classdays[y].classday){
+                    varClassday = classes[x].classdays[y].classday;
+                    for(z=0; z<classes[x].classdays[y].classhours.length; z++){
+                        if(selectedClasshour == classes[x].classdays[y].classhours[z].classhour){
+                            varClasshour = classes[x].classdays[y].classhours[z].classhour;
+                            for(a=0; a<classes[x].classdays[y].classhours[z].students.length; a++){
+                                
+                                idno = classes[x].classdays[y].classhours[z].students[a].idno;
+                                name = classes[x].classdays[y].classhours[z].students[a].name;
+                                demerit = classes[x].classdays[y].classhours[z].students[a].demerit;
+                                absent = classes[x].classdays[y].classhours[z].students[a].absent;
+                                report = classes[x].classdays[y].classhours[z].students[a].report;
+                                
+                                varRow = document.createElement("tr");
+                                
+                                varTd = document.createElement("td");
+                                varTdText = document.createTextNode(idno);
+                                varTd.appendChild(varTdText);
+                                varRow.appendChild(varTd);
+
+                                varTd = document.createElement("td");
+                                varTdText = document.createTextNode(name);
+                                varTd.appendChild(varTdText);
+                                varRow.appendChild(varTd);
+
+                                //demerit dropdown list
+                                varTd = document.createElement("td");
+                                varTdText = document.createTextNode(demerit);
+                                selectDemerit = document.createElement("select");
+                                selectDemerit.setAttribute("id", "selectdemerit");
+
+                                optionDefault = document.createElement("option");
+                                optionDefaultTxt =  document.createTextNode("---");
+                                optionDefault.appendChild(optionDefaultTxt);
+                                optionDefault.selected = true;
+
+                                optionDemerit = document.createElement("option");
+                                optionDemeritTxt =  document.createTextNode("late");
+                                optionDemerit.setAttribute("value", "late;"+idno+";"+varClasscode+";"+varClassday+";"+varClasshour);
+                                optionDemerit.appendChild(optionDemeritTxt);
+
+                                optionOthers = document.createElement("option");
+                                optionOthersTxt =  document.createTextNode("others");
+                                optionOthers.setAttribute("value", "others;"+idno);
+                                optionOthers.appendChild(optionOthersTxt); 
+
+                                selectDemerit.appendChild(optionDefault);
+                                selectDemerit.appendChild(optionDemerit);
+                                selectDemerit.appendChild(optionOthers);
+
+                                selectDemerit.addEventListener("change", setDemerit);
+
+                                varTd.appendChild(selectDemerit);
+
+
+                                varTd.appendChild(varTdText);
+                                varRow.appendChild(varTd);
+
+                                varTd = document.createElement("td");
+                                varTdText = document.createTextNode(absent);
+                                absentBox = document.createElement("input");
+                                absentBox.setAttribute("type", "checkbox");
+                                varTd.appendChild(absentBox);
+                                varRow.appendChild(varTd);
+
+                                varTd = document.createElement("td");
+                                varTdText = document.createTextNode(report);
+                                varTd.appendChild(varTdText);
+                                varRow.appendChild(varTd);
+
+                                studentList.appendChild(varRow);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    var existingStudentList = document.getElementById("studentlist");
+	if(existingStudentList){
+	    selectionBox.replaceChild(studentList, existingStudentList);
+    }else{
+		selectionBox.appendChild(studentList);
+	}
+}
